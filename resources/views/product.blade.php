d7702df02e5b724a581240d8fdadd6d3bbfc7508
Irfanbd <gourob83@gmail.com>
2021-10-20 19:46:56 +0600

    Trending-items bug fixed

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,9 +1,6 @@
-@extends('master');
-@section('content');
-    
-
-<div class=" custom-product ">
-   
+@extends('master')
+@section("content")
+<div class="custom-product">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -14,18 +11,18 @@
       
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
-          
-      @foreach ($products as $item)
-      <div class="item {{$item['id']==1?'active':''}}">
-        <img calss="slider-img" src="{{$item['gallery']}}" >
-        <div class="carousel-caption slider-text">
-          <h3>{{$item['name']}}</h3>
-          <p>{{$item['description']}}</p>
-        </div>
-      </div>
-          
-      @endforeach
-          
+          @foreach ($products as $item)
+        <div class="item {{$item['id']==1?'active':''}}">
+        <a href="detail/{{$item['id']}}">
+              <img class="slider-img" src="{{$item['gallery']}}">
+            <div class="carousel-caption slider-text">
+              <h3>{{$item['name']}}</h3>
+              <p>{{$item['description']}}</p>
+            </div>
+            </a>
+          </div>
+    
+          @endforeach
         </div>
       
         <!-- Left and right controls -->
@@ -37,6 +34,20 @@
           <span class="glyphicon glyphicon-chevron-right"></span>
           <span class="sr-only">Next</span>
         </a>
+      </div>
+      <div class="trending-wrapper">
+        <h3>Tredning Products</h3>
+        @foreach($products as $item)
+        <div class="trening-item">
+          <a href="detail/{{$item['id']}}">
+          <img class="trending-image" src="{{$item['gallery']}}">
+          <div class="">
+            <h3>{{$item['name']}}</h3>
+          </div>
+        </a>
+        </div>
+        @endforeach
+      </div>
       </div>
 </div>
 @endsection
