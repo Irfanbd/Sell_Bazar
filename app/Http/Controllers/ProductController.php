aa221567838575718e4e3154694d69247e7ec70b
Irfanbd <gourob83@gmail.com>
2021-10-20 23:42:39 +0600

    Cart and Login Logout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
+
 class ProductController extends Controller
 {
     function index()
@@ -18,11 +21,29 @@ return view('detail',['product'=>$data]);
     }
 
     function search(Request $req)
-{ $data=Product::where('name','like','%'.$req->input('query').'%')->get();
+{ $data=Product::
+    where('name','like','%'.$req->input('query').'%')
+    ->get();
     return view('search',['products'=>$data]);
 }
 function addToCart(Request $req)
+{ if($req->session()->has('user'))
+    {
+        $cart=new Cart;
+        $cart->user_id=$req->session()->get('user')['id'];
+        $cart->product_id=$req->product_id;
+        $cart->save();
+        return redirect('/');
+    }
+    else
+    {
+        return redirect('/login');
+    }
+    
+}
+ static function cartItem()
 {
-    return"Hello";
+$userId=Session::get('user')['id'];
+return Cart::where('user_id',$userId)->count();
 }
 }
